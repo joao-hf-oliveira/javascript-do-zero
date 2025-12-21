e494e206bd2ecd217b35934ebe125d3411f198f7
João Henrique Faustino de Oliveira <joao.hq.oliveira@gmail.com>
2025-12-21 16:42:32 -0300

    primeira interação com JavaScript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 console.log("Projeto JavaScript do Zero iniciado");
+
+const botaoIntro = document.getElementById("btn-intro");
+const mensagemIntro = document.getElementById("mensagem-intro");
+
+botaoIntro.addEventListener("click", function(){
+    mensagemIntro.textContent = "JavaScript está funcionando. "
+})
